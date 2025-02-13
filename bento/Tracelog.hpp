1228e08bd51df51ba18619ffed60e129d0969565
nasirebus <nasirebus@insicuri.net>
2025-02-13 16:42:04 +0700

    convert TraceLog into macro

--- bento/Tracelog.hpp
+++ bento/Tracelog.hpp
@@ -1,6 +1,12 @@
 #pragma once
+#include <cstdio>
 
 namespace ppx
 {
-  void TraceLog(const char *format, ...);
+  #ifndef NDEBUG
+    #define TraceLog(fmt, ...) fprintf(stderr, fmt"\n", ##__VA_ARGS__)
+  #else
+    #define TraceLog(fmt, ...) ((void)0)
+  #endif
+
 }
