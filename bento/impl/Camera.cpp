27d7f7d68edff13032bc995812b136b7f9e09a17
nasirebus <nasirebus@insicuri.net>
2025-02-13 16:45:33 +0700

    use Matrix static function

--- bento/impl/Camera.cpp
+++ bento/impl/Camera.cpp
@@ -3,49 +3,35 @@
 
 namespace ppx
 {
+
+  void Camera::Set(const Vec2 &offset, const Vec2 &target, const int16_t rotation, const f32 &zoom)
+  {
+    this->offset = offset;
+    this->target = target;
+    this->rotation = rotation;
+    this->zoom = zoom;
+  }
+
   Vec2 Camera::CameraToScreen(const Vec2 &position)
   {
-    Matrix translate(
-      1, 0, position.x,
-      0, 1, position.y,
-      0, 0, 1);
-
+    Matrix translate = Matrix::Translation(position.x, position.y);
     Matrix m = translate * GetMatrix();
     return Vec2(m.m02, m.m12);
   }
 
   Vec2 Camera::ScreenToScreen(const Vec2 &position)
   {
-    Matrix translate(
-      1, 0, position.x,
-      0, 1, position.y,
-      0, 0, 1);
-
+    Matrix translate = Matrix::Translation(position.x, position.y);
     Matrix m = translate * Matrix::Inverse(GetMatrix());
     return Vec2(m.m02, m.m12);
   }
 
   Matrix Camera::GetMatrix()
   {
-    Matrix m_origin(
-      1, 0, -target.x,
-      0, 1, -target.y,
-      0, 0, 1);
-
-    Matrix m_rotation(
-      math::cos(rotation), -math::sin(rotation), 0,
-      math::sin(rotation), math::cos(rotation), 0,
-      0, 0, 1);
-
-    Matrix m_scale(
-      zoom, 0, 0,
-      0, zoom, 0,
-      0, 0, 1);
-
-    Matrix m_translate(
-      1, 0, offset.x,
-      0, 1, offset.y,
-      0, 0, 1);
+    Matrix m_origin = Matrix::Translation(-target.x, -target.y);
+    Matrix m_rotation = Matrix::Rotation(rotation);
+    Matrix m_scale = Matrix::Scale(zoom, zoom);
+    Matrix m_translate = Matrix::Translation(offset.x, offset.y);
 
     return m_origin * (m_scale * m_rotation) * m_translate;
   }
