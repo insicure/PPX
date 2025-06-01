cfde32959357507a0e7a9481e1f37a4103cc6119
sillysagiri <sillysagiri@privacyrequired.com>
2025-06-01 08:09:43 +0700

    sillyimage bump v13

--- bento/SillyImage.hpp
+++ bento/SillyImage.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "FileData.hpp"
 #include <cstdint>
 
 namespace ppx
@@ -12,25 +11,12 @@ namespace ppx
     ImageType_INDEXED_4, // 2bpp
     ImageType_INDEXED_16, // 4bpp
     ImageType_INDEXED_256, // 8bpp
-    ImageType_INDEXED_32_A3, // 5bpp, Bit5-7: Alpha
-    ImageType_INDEXED_8_A5, // 3bpp, Bit3-7: Alpha
+    ImageType_INDEXED_32_A8, // 5bpp, Bit5-7: Alpha
+    ImageType_INDEXED_8_A32, // 3bpp, Bit3-7: Alpha
     ImageType_PALETTE_16,
     // ImageType_TILE_8x8,
     ImageType_INVALID,
   };
-
-  #pragma pack(push, 1) 
-  struct SillyImageMetadata {
-    uint64_t header;
-    uint8_t version;
-    uint8_t format;
-    uint8_t paletteid;
-    uint16_t width;
-    uint16_t height;
-    uint8_t compression;
-    uint32_t length;
-  };
-  #pragma pack(pop)
 
   class SillyImage
   {
@@ -38,19 +24,16 @@ namespace ppx
     uint8_t *data = nullptr;
     uint16_t width = 0;
     uint16_t height = 0;
-    uint8_t paletteid = 0;
-    ImageType format = ImageType_INVALID;
-
-    constexpr SillyImage()
-      : data(nullptr), width(0), height(0), paletteid(0), format(ImageType_INVALID), originaldata() { }
-
-    SillyImage(const char *filename);
+    uint16_t palette_count = 0;
+    uint32_t palette_hash = 0;
+    uint8_t *palette_data = nullptr;
+    uint8_t format = ImageType_INVALID;
 
     bool Load(const char *filename);
     void Unload();
     bool isValid();
 
   private:
-    FileData originaldata{};
+    uint8_t *temp_compress = nullptr;
   };
 }
--- bento/impl/SillyImage.cpp
+++ bento/impl/SillyImage.cpp
@@ -1,93 +1,176 @@
 #include "../SillyImage.hpp"
-#include "../FileData.hpp"
-#include "../Tracelog.hpp"
-#include "../Assert.hpp"
+#include "bento/Tracelog.hpp"
+#include "fastlz.h"
+#include <cstdio>
 #include <cstdlib>
 
-#define SILLYIMAGE_HEADER 0x676D69796C6C6973
+#define SILLYIMAGE_HEADER 7452728928599042419
 
 namespace ppx
 {
-
-  SillyImage::SillyImage(const char *filename)
-    : data(nullptr), width(0), height(0), paletteid(0), format(ImageType_INVALID), originaldata()
-  {
-    Load(filename);
-  }
-
   bool SillyImage::Load(const char *filename)
   {
+    if (isValid()) Unload();
 
-    bool res = false;
+    FILE *ptr_file = nullptr;
+    uint8_t *temp_compress = nullptr;
+    uint32_t original_size = 0;
+    uint32_t compress_size = 0;
+    bool success = false;
 
-    do
-    {
-      res = originaldata.Load(filename);
-      if (!res) break;
+    uint64_t head = 0;
 
-      // check if metadata exist
-      const bool cond = originaldata.length > sizeof(SillyImageMetadata);
-      res_sassert(res, cond, "SillyImage: length < SillyImageMetadata, %s", filename);
-      if (!res) break;
-
-      SillyImageMetadata meta = *(SillyImageMetadata*)originaldata.data;
-
-      // check header
-      res_sassert(res, meta.header == SILLYIMAGE_HEADER, "SillyImage: invalid meta header, %s", filename);
-      if (!res) break;
-
-      // check version
-      res_sassert(res, meta.version == 0, "SillyImage: invalid meta version, %s", filename);
-      if (!res) break;
-
-      // check format
-      res_sassert(res, meta.format <= 7, "SillyImage: invalid meta format, %s", filename);
-      switch (meta.format) {
-        case 0: format = ImageType_R8G8B8A8; break;
-        case 1: format = ImageType_R5G5B5A1; break;
-        case 2: format = ImageType_INDEXED_4; break;
-        case 3: format = ImageType_INDEXED_16; break;
-        case 4: format = ImageType_INDEXED_256; break;
-        case 5: format = ImageType_INDEXED_32_A3; break;
-        case 6: format = ImageType_INDEXED_8_A5; break;
-        case 7: format = ImageType_PALETTE_16; break;
-        
-        default: res = false; break;
-      }
-      if (!res) break;
-
-      paletteid = meta.paletteid;
-      width = meta.width;
-      height = meta.height;
-      data = (originaldata.data+sizeof(SillyImageMetadata));
-
-    } while(0);
-
-    if (!res)
-    {
-      TraceLog("SillyImage: load failed, %s", filename);
-      Unload();
-    }
-    else
-    {
-      // TraceLog("SillyImage: load success, (%ux%u) pid:%u %s", width, height, paletteid, filename);
+    // Validate filename
+    if (!filename || filename[0] == '\0') {
+      TraceLog("SillyImage: Empty filename");
+      return false;
     }
 
-    return res;
+    // Open file
+    ptr_file = fopen(filename, "rb");
+    if (!ptr_file) {
+      TraceLog("SillyImage: fopen failed: %s", filename);
+      goto cleanup;
+    }
+
+    // Verify header
+    if (fread(&head, sizeof(head), 1, ptr_file) != 1) {
+      TraceLog("SillyImage: Failed read header: %s", filename);
+      goto cleanup;
+    }
+
+    if (head != SILLYIMAGE_HEADER) {
+      TraceLog("SillyImage: Invalid header: %s", filename);
+      goto cleanup;
+    }
+
+    // Verify version
+    uint8_t version;
+    if (fread(&version, sizeof(version), 1, ptr_file) != 1) {
+      TraceLog("SillyImage: Failed read version: %s", filename);
+      goto cleanup;
+    }
+
+    if (version != 13) {
+      TraceLog("SillyImage: Unsupported version: %s", filename);
+      goto cleanup;
+    }
+
+    // Verify format
+    uint8_t raw_format;
+    if (fread(&raw_format, sizeof(raw_format), 1, ptr_file) != 1) {
+      TraceLog("SillyImage: Failed read format: %s", filename);
+      goto cleanup;
+    }
+    
+    if (raw_format > static_cast<uint8_t>(ImageType::ImageType_PALETTE_16)) {
+      TraceLog("SillyImage: Invalid format: %s", filename);
+      goto cleanup;
+    }
+
+    switch (raw_format) {
+      case 0: format = ImageType_R8G8B8A8; break;
+      case 1: format = ImageType_R5G5B5A1; break;
+      case 2: format = ImageType_INDEXED_4; break;
+      case 3: format = ImageType_INDEXED_16; break;
+      case 4: format = ImageType_INDEXED_256; break;
+      case 5: format = ImageType_INDEXED_32_A8; break;
+      case 6: format = ImageType_INDEXED_8_A32; break;
+      case 7: format = ImageType_PALETTE_16; break;
+    }
+
+    // Read dimensions
+    if (fread(&width, sizeof(width), 1, ptr_file) != 1) {
+      TraceLog("SillyImage: Failed read width: %s", filename);
+      goto cleanup;
+    }
+    
+    if (fread(&height, sizeof(height), 1, ptr_file) != 1) {
+      TraceLog("SillyImage: Failed read height: %s", filename);
+      goto cleanup;
+    }
+
+    // Read palette
+    if (fread(&palette_count, sizeof(palette_count), 1, ptr_file) != 1) {
+      TraceLog("SillyImage: Failed read palette_count: %s", filename);
+      goto cleanup;
+    }
+
+    if (fread(&palette_hash, sizeof(palette_hash), 1, ptr_file) != 1) {
+      TraceLog("SillyImage: Failed read palette_hash: %s", filename);
+      goto cleanup;
+    }
+
+    // Read buffer size
+    if (fread(&original_size, sizeof(original_size), 1, ptr_file) != 1) {
+      TraceLog("SillyImage: Failed read original_size: %s", filename);
+      goto cleanup;
+    }
+
+    if (fread(&compress_size, sizeof(compress_size), 1, ptr_file) != 1) {
+      TraceLog("SillyImage: Failed read compress_size: %s", filename);
+      goto cleanup;
+    }
+
+    // Allocate palette buffer
+    palette_data = static_cast<uint8_t*>(malloc(palette_count * sizeof(uint16_t)));
+    if (!palette_data) {
+      TraceLog("SillyImage: malloc palette_data failed: %s", filename);
+      goto cleanup;
+    }
+
+    if (fread(palette_data, sizeof(uint16_t), palette_count, ptr_file) != palette_count) {
+      TraceLog("SillyImage: Failed read palette_data: %s", filename);
+      goto cleanup;
+    }
+
+    // Allocate image buffers
+    data = static_cast<uint8_t*>(malloc(original_size));
+    if (!data) {
+      TraceLog("SillyImage: malloc data failed: %s", filename);
+      goto cleanup;
+    }
+
+    temp_compress = static_cast<uint8_t*>(malloc(compress_size));
+    if (!temp_compress) {
+      TraceLog("SillyImage: malloc temp_compress failed: %s", filename);
+      goto cleanup;
+    }
+
+    // Decompress image data
+    if (fread(temp_compress, 1, compress_size, ptr_file) != compress_size) {
+      TraceLog("SillyImage: Failed read compressed data: %s", filename);
+      goto cleanup;
+    }
+
+    if (!fastlz_decompress(temp_compress, compress_size, data, original_size)) {
+      TraceLog("SillyImage: Decompression failed: %s", filename);
+      goto cleanup;
+    }
+
+    success = true;
+    TraceLog("SillyImage: Loaded successfully: %s", filename);
+
+cleanup:
+    if (temp_compress) free(temp_compress);
+    if (ptr_file) fclose(ptr_file);
+
+    if (!success) Unload();
+    return success;
   }
 
   void SillyImage::Unload()
   {
-    if (originaldata.isValid())
-    {
-      originaldata.Unload();
-      // TraceLog("SillyImage: unloaded");
-    }
-
+    free(data);
     data = nullptr;
+
+    free(palette_data);
+    palette_data = nullptr;
+
+    width = height = 0;
+    palette_count = 0;
+    palette_hash = 0;
     format = ImageType_INVALID;
-    width = 0;
-    height = 0;
   }
 
   bool SillyImage::isValid()
